ab1ee5701f454278a291b5f4b266bab52d13665d
Abduvohid <vohid4970@gmail.com>
2023-07-19 09:53:56 +0500

    first commit

---
+++ src/components/PhoneInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <input type="number" placeholder="+998 (00) 000-00-00 " />
+</template>
+
+<style>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+</style>
